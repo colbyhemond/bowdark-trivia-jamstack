2b791babc48f3a0aaba52d9a6323e0ff1d7d8555
Colby Hemond <hemond.colby@gmail.com>
2022-12-24 15:03:17 -0500

    update helllo api to troubleshoot functions

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -8,73 +8,7 @@ export default async function handler(req, res) {
 
   //General
   console.log('ℹ️ Request Received');
-  const client = await clientPromise;
-  const db = client.db(process.env.MONGODB_DB);
-  
-  switch (req.method) {
-    case "POST":
-        try {
-            let bodyObject = req.body ? JSON.parse(req.body) : null;
-            console.log('POST Request Received');
-            console.log(req.body);
 
-            
-            res.status(201).json(req)
-            return
-
-        } catch (error) {
-          console.log(error);
-          res.status(500).json(error)
-        }
-      break
-    case "GET":
-        try {
-            console.log('GET Request Received');
-
-    
-
-            res.status(201).json(req)
-            return
-
-        } catch (error) {
-          console.log(error);
-          res.status(500).json(error)
-        }
-      break
-    case "PUT":
-        try {
-
-            console.log('PUT Request Received');
-            res.status(201).json(req)
-            return
-
-        } catch (error) {
-          console.log(error);
-          res.status(500).json(error)
-        }
-      break
-    case 'DELETE':
-      try {
-
-            console.log('DELETE Request Received');
-
-            console.log();
-            const body = JSON.parse(req.body)
-
-
-            if (item.deletedCount === 1) {
-                res.status(204).json('')
-                return
-            } else {
-                res.status(404).json('')
-                return
-            }
-            
-
-      } catch (error) {
-        console.log(error);
-        res.status(500).json(error)
-      }
-    break
-  }
+  res.status(201).json({msg: 'Hello threre! It worked!'})
+  return
 }
